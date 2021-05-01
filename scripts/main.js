f4ab4361d217794bca8ad551d9f16ce889cb7570
HollywoodOKC <T_Lane11@yahoo.com>
2021-05-01 17:16:47 -0500

    01MAY21

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
 alert("Welcome to the Hotel California");
 
+function showTime() {
+  const date = new Date();
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  let time = `${h}:${m}:${s} ${session}`
+  document.getElementById("time").innerText = time;
+  document.getElementById("time").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
 let hotel = {
   name: 'Hotel California',
   rooms: 200,
@@ -17,21 +46,21 @@ elName.textContent = hotel.name;
 let elRooms = document.getElementById('rooms');
 elRooms.textContent = hotel.checkAvailability();
 
-let myTime = new Date();
-let myHour = myTime.getHours();
-document.getElementById("name").innerHTML = "The Hotel California" + "myHour";
 
 function timeOfDay() {
   const theTime = new Date();
   const theHour = theTime.getHours();
 
   if (theHour >= 6 && theHour <= 11) {
-    document.getElementById("time").innerHTML = "Good Morning";
+    document.getElementById("clockDisplay").innerHTML = `Good Morning`;
   } else if (theHour >= 12 && theHour <= 18) {
-    document.getElementById("time").innerHTML = "Good Afternoon";
+    document.getElementById("clockDisplay").innerHTML = "Good Afternoon";
   } else if (theHour >= 19 && theHour <= 23) {
-    document.getElementById("time").innerHTML = "Good Evening";
+    document.getElementById("clockDisplay").innerHTML = "Good Evening";
   } else {
-    document.getElementById("time").innerHTML = "Good Day";
+    document.getElementById("clockDisplay").innerHTML = "Good Day";
   }
+  setTimeout(timeOfDay, 1000);
 }
+
+timeOfDay();
