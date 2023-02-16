07e85d2bf3066fdce119de89ee43bc3acc4403c3
HollywoodOKC <T_Lane11@yahoo.com>
2023-02-16 15:18:49 -0600

    Added blog.

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,14 +1,28 @@
-const buttons = document.getElementById('button');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
 
 const changeButton = () => {
-  buttons.style.backgroundColor = 'black';
-  buttons.style.color = 'white';
+  button1.style.backgroundColor = 'black';
+  button1.style.color = 'white';
+}
+
+const changeButton2 = () => {
+  button2.style.backgroundColor = 'black';
+  button2.style.color = 'white';
 }
 
 const changeBk = () => {
-  buttons.style.backgroundColor = 'white';
-  buttons.style.color = 'black';
+  button1.style.backgroundColor = 'white';
+  button1.style.color = 'black';
 }
 
-buttons.addEventListener('mouseover', changeButton);
-buttons.addEventListener('mouseout', changeBk);
+const changeBk2 = () => {
+  button2.style.backgroundColor = 'white';
+  button2.style.color = 'black';
+}
+
+button1.addEventListener('mouseover', changeButton);
+button1.addEventListener('mouseout', changeBk);
+button2.addEventListener('mouseover', changeButton2);
+button2.addEventListener('mouseout', changeBk2);
