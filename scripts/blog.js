b97de93f0a39b2bb5b74b05b403c434b739cdad2
HollywoodOKC <T_Lane11@yahoo.com>
2023-02-16 19:19:45 -0600

    Added blog post.

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,5 +1,6 @@
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
 
 
 const changeButton = () => {
@@ -12,6 +13,11 @@ const changeButton2 = () => {
   button2.style.color = 'white';
 }
 
+const changeButton3 = () => {
+  button3.style.backgroundColor = 'black';
+  button3.style.color = 'white';
+}
+
 const changeBk = () => {
   button1.style.backgroundColor = 'white';
   button1.style.color = 'black';
@@ -22,7 +28,14 @@ const changeBk2 = () => {
   button2.style.color = 'black';
 }
 
+const changeBk3 = () => {
+  button3.style.backgroundColor = 'white';
+  button3.style.color = 'black';
+}
+
 button1.addEventListener('mouseover', changeButton);
 button1.addEventListener('mouseout', changeBk);
 button2.addEventListener('mouseover', changeButton2);
 button2.addEventListener('mouseout', changeBk2);
+button3.addEventListener('mouseover', changeButton3);
+button3.addEventListener('mouseout', changeBk3);
