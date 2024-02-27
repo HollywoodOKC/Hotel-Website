cc4cc69c29c89a9a4176303070ac39711ef4a553
HollywoodOKC <T_Lane11@yahoo.com>
2024-02-27 04:27:09 -0600

    feat: Added room picture with room selection

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,11 +1,13 @@
 //alert("Welcome to the Hotel California!");
-
+const roomPic = document.getElementById('roomPic');
+const form = document.getElementById('form');
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById('roomType').innerHTML = "Select Room";
 
 const theDay = () => {
   d = new Date();
   document.getElementById("clockDisplay").innerHTML = days[d.getDay()];
-}
+};
 
 theDay();
 
@@ -17,25 +19,34 @@ const showTime = () => {
   const wholeTime = `${hours}:${mins}:${secs}`;
   document.getElementById("time").innerHTML = wholeTime;
   setTimeout(showTime, 1000);
-}
+};
 
 showTime();
+
 
 const showRoom = () => {
   let rooms;
   if (document.getElementById("monroe").checked) {
     rooms = document.getElementById("roomType").innerHTML = "Monroe Suite";
+    roomPic.src = "./resources/images/monroe suite.jpg";
+    roomPic.alt = "The Monroe Suite";
   } else if (document.getElementById("tropicana").checked) {
     rooms = document.getElementById("roomType").innerHTML = "Tropicana Suite";
+    roomPic.src = "./resources/images/tropicana suite.jpg";
+    roomPic.alt = "The Tropicana Suite";
   } else if (document.getElementById("roosevelt").checked) {
     rooms = document.getElementById("roomType").innerHTML = "Roosevelt Suite";
+    roomPic.src = "./resources/images/roosevelt suite.jpeg";
+    roomPic.alt = "The Roosevelt Suite";
   } else if (document.getElementById("normal").checked) {
     rooms = document.getElementById("roomType").innerHTML = "Normal H.C. Suite";
+    roomPic.src = "./resources/images/normal suite.jpg";
+    roomPic.alt = "Normal HC Suite";
   } else {
-    rooms = document.getElementById('roomType').innerHTML = "Select Room";
+
+    roomPic.src = "";
   }
   document.getElementById('roomType').innerHTML = rooms;
-  setTimeout(showRoom, 1000);
-}
+};
 
-showRoom();
+form.addEventListener('click', showRoom);
